f119dfaab8e45f494ff03075cbf7066e777ec98c
Frank H Mcmanus <frank@thefrankharvey.com>
2018-02-28 03:23:25 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -12,5 +12,5 @@ end
 
 def assign_rooms(arr)
   room_assignments = []
-  arr.each_with_index {|value, index| room_assignments.push(value)}
+  arr.each_with_index {|value, index| room_assignments.push(index)}
 end
